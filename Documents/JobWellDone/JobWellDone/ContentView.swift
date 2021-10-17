7bc1d396c56e0e4554dda205fe7173ebe47c741a
iOSDev1729 <iosintials1729@gmail.com>
2021-10-17 20:07:17 +0530

    VStack & Image

--- Documents/JobWellDone/JobWellDone/ContentView.swift
+++ Documents/JobWellDone/JobWellDone/ContentView.swift
@@ -10,27 +10,34 @@ import SwiftUI
 struct ContentView: View {
    
     @State var myArray = ["ghg","yhty","hdty","hrhr","rtbb","rtyert"]
+    //@State var viewModel = ViewModel() // class object or reference or instance
     
     
     var body: some View {
-        Color.green
-       
+        HStack {
+            VStack {
+        Color.orange
+            .padding(.all)
+        
+        Text("Radhika").padding()
+        Image("balatripurasundari")
+        
         List(myArray, id: \.self) {Text($0)}
         
-        List {
-            Image(systemName: "list.dash")
-            Text("Second")
-            Button(action: {
-                    print("Action")
-            }) {
-                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-            }
-            ForEach(1..<50) { index in
-                Text("\(index)")
-            }
-        }
-        .font(.headline)
-        .clipped()
+                List {
+                    Image(systemName: "list.dash")
+                    Text("Second")
+                    Button(action: {
+                        print("Action")
+                    }) {
+                        Text("Button")
+                    }
+                    ForEach(1..<50) { index in
+                        Text("\(index)")
+                    }
+                }
+                .font(.headline)
+                .clipped()
        
             }
     
@@ -38,9 +45,23 @@ struct ContentView: View {
            // .padding()
     }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+/*extension ContentView {
+    class ViewModel:ObservedObject {
+        @Published var users = [User]()
+        
+}
+}*/
     }
 }
 
+//MARK:
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+            
+    }
+}
+
+    
